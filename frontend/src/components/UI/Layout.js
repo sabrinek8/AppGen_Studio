16770dd1787aa293abf021399c15b514d0b62f9d
Sabrine KAMOUN <sabrine.kamoun@etudiant-fst.utm.tn>
2025-07-19 15:30:35 +0200

    Change Layout Style

--- frontend/src/components/UI/Layout.js
+++ frontend/src/components/UI/Layout.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
 export const Layout = ({ children }) => (
-  <div style={{
+  <div className="bg-body" style={{
     minHeight: '100vh',
-    background: 'linear-gradient(135deg, #667eea 0%, #764ba2 100%)',
-    fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, sans-serif'
+    fontFamily: '-apple-system, Helvetica Neue, "Segoe UI", Roboto, sans-serif'
   }}>
     {children}
   </div>
